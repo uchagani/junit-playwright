16b9d297249316692fa83e2b134472b2bd0b551e
Umair Chagani <umair.chagani@gmail.com>
2022-03-31 22:32:00 -0400

    add playwright create tests to github actions

--- src/test/java/io/github/uchagani/jp/InjectPlaywrightTest.java
+++ src/test/java/io/github/uchagani/jp/InjectPlaywrightTest.java
@@ -1,10 +1,16 @@
 package io.github.uchagani.jp;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.platform.testkit.engine.EngineTestKit;
 
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
+// This needs to be run serially until https://github.com/microsoft/playwright-java/issues/878 is fixed
+@Tag("playwrightCreate")
+@Execution(ExecutionMode.SAME_THREAD)
 public class InjectPlaywrightTest {
     @Test
     void injectPlaywrightTestRunsSuccessfully() {
